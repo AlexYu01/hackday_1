9adcd0c081c8eab5b22b5b89be19852e1de27d71
AlexYu01 <tengalexyu@gmail.com>
2019-05-10 15:25:14 -0700

    Integrate timer

--- ClickApp.py
+++ ClickApp.py
@@ -1,8 +1,11 @@
 from kivy.app import App
 from kivy.uix.button import Button
-from kivy.properties import ObjectProperty
-from kivy.uix.widget import Widget
+from kivy.animation import Animation
+from kivy.properties import ObjectProperty, NumericProperty
 from kivy.config import Config
+from kivy.uix.label import Label
+from kivy.clock import Clock
+from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
 Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
 Config.set('graphics', 'resizable', False)
 
@@ -23,34 +26,37 @@ class HardButton(Button):
     pass
 
 
-#class Timer():
-#    def build(self):
-#        self.modes = ('%S:')
-#        self.mode = 0
-#        self.main_box = BoxLayout(orientation='vertical')
-#        
-#        self.button = Button(text='label', font_size=100)
-#        self.main_box.add_widget(self.button)
-#    
-#        self.button.bind(on_press=self.tap)
-#        Clock.schedule_interval(self.timer, 0.01)
-#        
-#        return self.main_box
-#    
-#    def tap(self, button):
-#        if self.mode +1 == len(self.modes):
-#            self.mode = 0
-#        else:
-#            self.mode +=1
-#    
-#    def timer(self, dt):
-#        now = datetime.datetime.now()
-#        self.button.text = now.strftime(self.modes[self.mode])
-#        if self.mode == 2:
-#            self.button.text += str(now.microsecond)[:3]
+class CountDown(Label):
+    anim = None
+    time_counter = NumericProperty(20)
 
-class Container(Widget):
+    def start(self):
+        Animation.cancel_all(self)  # stop any current animations
+        self.anim = Animation(time_counter=0, duration=self.time_counter)
+        self.anim.start(self)
+
+
+class Mode(Screen):
+
+    def change_screen(self, count_down):
+        self.manager.current = 'clicker'
+        self.manager.ids.click_away.count = CountDown()
+        self.manager.ids.click_away.count.time_counter = count_down
+        self.manager.ids.click_away.count.start()
+
+
+class ClickAway(Screen):
     addButton = ObjectProperty()
+    countDown = ObjectProperty()
+    count = None
+
+    def __init__(self, **kwargs):
+        super(ClickAway, self).__init__(**kwargs)
+        self.count = CountDown()
+        Clock.schedule_interval(self.update, 1.0 / 60.0)
+
+    def update(self, *args):
+        self.countDown.text = str(round(self.count.time_counter, 1))
 
     def add_one(self):
         value = int(self.addButton.text)
@@ -60,12 +66,17 @@ class Container(Widget):
         self.addButton.disabled = False
 
 
+class Manager(ScreenManager):
+
+    mode_screen = ObjectProperty(None)
+    click_away = ObjectProperty(None)
+
+
 class ClickApp(App):
     def build(self):
         self.title = 'Away ISA!'
-        stuff = Container()
-        stuff.addButton.disabled = True
-        return stuff
+        screen_manager = Manager(transition=SlideTransition())
+        return screen_manager
 
 
 if __name__ == "__main__":
