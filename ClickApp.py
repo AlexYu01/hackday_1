cd2ee15c1f80a5c0cea76c64cb672757ea1c5782
AlexYu01 <tengalexyu@gmail.com>
2019-05-10 10:36:40 -0700

    Add screen switches
    - Screen switches between difficulty selection menu and clicker screen

--- ClickApp.py
+++ ClickApp.py
@@ -1,30 +1,33 @@
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.properties import ObjectProperty
-from kivy.uix.widget import Widget
 from kivy.config import Config
 Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
 Config.set('graphics', 'resizable', False)
+from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
 
 
 class AddButton(Button):
     pass
 
-
 class EasyButton(Button):
     pass
-
 
 class NormalButton(Button):
     pass
 
-
 class HardButton(Button):
     pass
 
+class Mode(Screen):
 
-class Container(Widget):
+    def change_screen(self, count_down):
+        self.manager.current = 'clicker'
+        self.manager.ids.click_away.timer = count_down
+
+class ClickAway(Screen):
     addButton = ObjectProperty()
+    timer = 0
 
     def add_one(self):
         value = int(self.addButton.text)
@@ -33,14 +36,16 @@ class Container(Widget):
     def enable(self):
         self.addButton.disabled = False
 
+class Manager(ScreenManager):
+
+    mode_screen = ObjectProperty(None)
+    click_away = ObjectProperty(None)
 
 class ClickApp(App):
     def build(self):
         self.title = 'Away ISA!'
-        stuff = Container()
-        stuff.addButton.disabled = True
-        return stuff
-
+        screen_manager = Manager(transition=SlideTransition())
+        return screen_manager
 
 if __name__ == "__main__":
     app = ClickApp()
