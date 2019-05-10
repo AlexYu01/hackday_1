2686799be1dadbbd205c26853a0b30bc7b4f214f
AlexYu01 <tengalexyu@gmail.com>
2019-05-10 01:30:16 -0700

    Add difficulty buttons
    - Add difficulty buttons
    - AddButton is disabled until a difficulty button is pressed.

--- ClickApp.py
+++ ClickApp.py
@@ -1,25 +1,45 @@
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.properties import ObjectProperty
-from kivy.uix.gridlayout import GridLayout
+from kivy.uix.widget import Widget
+from kivy.config import Config
+Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
+Config.set('graphics', 'resizable', False)
 
 
 class AddButton(Button):
     pass
 
 
-class Container(GridLayout):
+class EasyButton(Button):
+    pass
+
+
+class NormalButton(Button):
+    pass
+
+
+class HardButton(Button):
+    pass
+
+
+class Container(Widget):
     addButton = ObjectProperty()
 
     def add_one(self):
         value = int(self.addButton.text)
         self.addButton.text = str(value + 1)
 
+    def enable(self):
+        self.addButton.disabled = False
+
 
 class ClickApp(App):
     def build(self):
         self.title = 'Away ISA!'
-        return Container()
+        stuff = Container()
+        stuff.addButton.disabled = True
+        return stuff
 
 
 if __name__ == "__main__":
